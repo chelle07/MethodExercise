5bf9ca052914422c7938f12a11ef405997d10dac
chelle07 <81632800+chelle07@users.noreply.github.com>
2021-12-04 11:00:19 -0700

    began user questions

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello - what is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} what is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a pretty color {userName}, What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"I love {animal} too, What is your favorite band?");
+            var band = Console.ReadLine();
+
+
+        }
+    }
+}
