b2180bd64ee338c6b4e906bf32625d81aabc8d36
chelle07 <81632800+chelle07@users.noreply.github.com>
2022-05-06 00:53:26 -0700

    finished project

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -4,8 +4,28 @@ namespace MethodExercise
 {
     class Program
     {
+        public static int sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+        
+        public static int divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
         static void Main(string[] args)
         {
+
+            var amountOfCars = sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+            var numberOfDucks = divide(12, 6);
             Console.WriteLine("Hello - what is your name?");
             var userName = Console.ReadLine();
 
@@ -20,10 +40,18 @@ namespace MethodExercise
 
             Console.WriteLine($"Thanks {userName}! Here is your profile.");
 
+            Console.WriteLine($"-----------------------------------");
+
             Console.WriteLine($"Name: {userName}");
             Console.WriteLine($"Favorite Color: {color}");
             Console.WriteLine($"Favorite Animal: {animal}");
             Console.WriteLine($"Favorite Band: {band}");
+
+
+
+
+
+
         }
     }
 }
